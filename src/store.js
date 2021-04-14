2462ec602ab41494d2d0730aa9698f7c8e218308
gamzeumac <umacgamze49@gmail.com>
2021-04-14 15:28:09 +0200

    bugs

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, compose} from "redux";
-import {thunk} from "redux-thunk";
+import thunk from "redux-thunk";
 import { composeWidthDevTools} from "redux-devtools-extension";
 import rootReducer from "./reducers/rootReducer";
 
