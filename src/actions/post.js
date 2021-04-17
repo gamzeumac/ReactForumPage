0b4fe0327dd77a580fa586bad8ef015552234d74
gamzeumac <umacgamze49@gmail.com>
2021-04-17 02:56:34 +0200

    AddPostForm zurückwieder

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,12 +1,13 @@
-/* import * as api from "../api/index";
-import { FETCH_POSTS,FETCH_SINGLE_POST } from "./types.js";
+import * as api from "../api/index";
+import { FETCH_POSTS,FETCH_SINGLE_POST ,CREATE_POST,
+  UPDATE_POST,DELETE_POST} from "./types.js";
 
 
 export const fetchPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({
-      type: types.FETCH_POSTS,
+      type: FETCH_POSTS,
       payload: data,
     });
   } catch (error) {
@@ -18,7 +19,7 @@ export const fetchSinglePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.fetchSinglePost(id);
     dispatch({
-      type: types.FETCH_SINGLE_POST,
+      type: FETCH_SINGLE_POST,
       payload: data,
     });
   } catch (error) {
@@ -30,7 +31,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
     dispatch({
-      type: types.CREATE_POST,
+      type: CREATE_POST,
       payload: data,
     });
   } catch (error) {
@@ -42,7 +43,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: types.UPDATE_POST, payload: data });
+    dispatch({ type: UPDATE_POST, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -52,10 +53,10 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.deletePost(id);
     dispatch({
-      type: types.DELETE_POST,
+      type: DELETE_POST,
       payload: data._id,
     });
   } catch (error) {
     console.log(error);
   }
-}; */
+};
